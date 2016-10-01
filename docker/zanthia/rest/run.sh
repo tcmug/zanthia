1199091688eb4db716aafa6535a4e0ce62b7848f
Teemu Merikoski <teemu.merikoski@futurice.com>
2016-10-01 08:41:58 +0300

    Change file owner.

--- docker/zanthia/rest/run.sh
+++ docker/zanthia/rest/run.sh
@@ -9,7 +9,7 @@ done
 chmod o+w /etc/apache2/vhosts
 
 cp -rf /zanthia-shared/git_rsa /srv/rest-server/.ssh/id_rsa
-chown -R gitolite:gitolite /srv/rest-server/.ssh
+chown -R www:www /srv/rest-server/.ssh
 chmod 400 /srv/rest-server/.ssh/id_rsa
 
 uwsgi --plugins-dir /usr/lib/uwsgi --ini /srv/rest-server/uwsgi.ini
